049adb22159cabac2f2a5e944bd2e1a9d1492449
varun2200 <shahvarun391@gmail.com>
2024-02-12 13:54:00 +0530

    multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 } from "cloudinary";
+
+import fs from "fs";
+
+
+          
+cloudinary.config({ 
+  cloud_name:process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
+});
+
+const UploadOnCloudinary=async(filePath)=>{
+    try {
+        if(!filePath)  return null;
+        const response =await cloudinary.uploader.upload(filePath,{
+            resource_type:"auto"
+        });
+        console.log("File is uploaded on the cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filePath);
+        return null;
+    }
+}
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });

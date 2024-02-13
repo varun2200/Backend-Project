c082f9a335a1f979363a8cc40fe155dde659f908
varun2200 <shahvarun391@gmail.com>
2024-02-13 18:24:39 +0530

    user controllers and routes

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,11 +1,17 @@
-const asynchandler=(func)=> async(req,res,next)=>{
-    try {
-        await func(req,res,next)
-    } catch (error) {
-        res.send(err.code||400).json({
-            success:false,
-            message:err.message
-        })
+// const asynchandler=(func)=> async(req,res,next)=>{
+//     try {
+//         return await func(req,res,next)
+//     } catch (error) {
+//         res.send(err.code||400).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
